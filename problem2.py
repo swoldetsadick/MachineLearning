5bd7126bcb99c101535e69a46c1f36192d931c58
swoldetsadick <kebrab67@gmail.com>
2017-04-08 02:24:17 +0200

    first commit for problem2.py

---
+++ problem2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" problem2.py: This module implements a Linear Regression example(edx.org - ColumbiaX - AI Week 7)"""
+
+__author__ = "Selam Getachew Woldetsadick"
+__copyright__ = "Copyright (c) 2017 Selam Getachew Woldetsadick"
+
+
